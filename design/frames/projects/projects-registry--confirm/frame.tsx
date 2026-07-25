251da12953c7c12e274c472b63012c65946f77c0
Liam Vinberg <vinberg.liam@gmail.com>
2026-07-25 17:39:39 +0200

    design: settle the projects registry on corner dots and undo

--- design/frames/projects/projects-registry--confirm/frame.tsx
+++
@@ -1,5 +0,0 @@
-import { ProjectsRegistryScreen } from "../../../shared/ui/projects-registry-screen";
-
-export default function ProjectsRegistryConfirmFrame() {
-	return <ProjectsRegistryScreen canvasTarget="spool-canvas--live" menuTrigger="corner" removal="confirm" />;
-}
