bcfb533794e030212812201164b4bf95933d3545
Liam Vinberg <vinberg.liam@gmail.com>
2026-09-02 22:43:45 +0200

    design: 8 removed, 3 files

--- design/frames/explore/new-project/new-project-query--created/frame.tsx
+++
@@ -1,7 +0,0 @@
-// The folder is made, design/ is scaffolded, the tab is open on it: the query
-// that found nothing became the name.
-import { SpoolEmptyScreen } from "shared/ui/spool/empty-screen";
-
-export default function NewProjectQueryCreatedFrame() {
-	return <SpoolEmptyScreen project="tvarso" homeTarget="new-project-query" />;
-}
--- design/frames/explore/new-project/new-project-row--created/frame.tsx
+++
@@ -1,7 +0,0 @@
-// The folder is made, design/ is scaffolded, the tab is open on it: what the
-// picker was ever for, without Finder in the middle.
-import { SpoolEmptyScreen } from "shared/ui/spool/empty-screen";
-
-export default function NewProjectRowCreatedFrame() {
-	return <SpoolEmptyScreen project="tvarso" homeTarget="new-project-row" />;
-}
